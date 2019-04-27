9dd4fe5dce35e929758a6b5b33f35d3add5055ce
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-04-26 22:33:54 -0500

    Add an extra log

--- store/concurrent.go
+++ store/concurrent.go
@@ -8,6 +8,7 @@ import (
 // ConcurrentSave is a simple wrapper around DataStoreWrapper#Save. It is meant to ease calling from a `go` statement. The WaitGroup must be Added to before calling this function.
 func ConcurrentSave(n map[string]int, dataStore *DataStoreWrapper, wg *sync.WaitGroup) {
 	defer wg.Done()
+	log.Println("Starting save")
 
 	err := dataStore.Save(n)
 	if err != nil {
